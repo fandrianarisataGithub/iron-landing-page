72ab6bfe2422e8914e05f2ca7a0460884335addb
fenitriniaina <git.fandrianarisata@gmail.com>
2024-11-01 15:55:11 +0300

    step 2 and 3

---
+++ animation-scoll-h-infinte.js
@@ -0,0 +1,93 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    const mainSection = document.querySelector('.main-section');
+    const scrollBlock = document.querySelector('.scroll-block');
+  
+    // Créer un observer pour surveiller si la section principale est complètement visible
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+            console.log(entry)
+          if (entry.isIntersecting) {
+            console.log('visible')
+            // La section est complètement visible
+            document.body.style.overflow = 'hidden'; // Bloquer le défilement de la page
+            scrollBlock.style.overflowY = 'auto'; // Activer le défilement interne
+          } else {
+            // La section n'est pas complètement visible
+            console.log('not visible')
+            document.body.style.overflow = 'auto'; // Réactiver le défilement de la page
+            scrollBlock.style.overflowY = 'hidden'; // Désactiver le défilement interne
+          }
+        });
+      },
+      {
+        threshold: 1.0 // Déclencher seulement lorsque 100% de la section est visible
+      }
+    );
+  
+    // Observer la section principale
+    observer.observe(mainSection);
+  
+    // Gérer le défilement du sous-bloc pour réactiver le défilement de la page
+    scrollBlock.addEventListener('scroll', () => {
+        console.log('scroll')
+        if (scrollBlock.scrollTop === 0 || scrollBlock.scrollTop + scrollBlock.clientHeight >= scrollBlock.scrollHeight) {
+            document.body.style.overflow = 'auto'; // Réactiver le défilement de la page
+            scrollBlock.style.overflowY = 'hidden'; // Désactiver le défilement interne
+        }
+    });
+});*/
+
+const scrollers1 = document.querySelectorAll(".bloc-text-slide");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers1.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".text-to-flex");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+  
+const scrollers2 = document.querySelectorAll(".bloc-text-slide-with-image");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers2.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".text-to-flex");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
